347fa1b16c3fc1b2f84e52cfedaa31981b05f2ca
Shalabh Aggarwal <shalabh7777@gmail.com>
2014-09-14 20:55:32 +0530

    Chapter 1 - Recipe 'Creating modular web app with Blueprints'

--- my_app/hello/views.py
+++ my_app/hello/views.py
@@ -1,18 +1,21 @@
-from my_app import app
+from flask import Blueprint
 from my_app.hello.models import MESSAGES
 
-@app.route('/')
-@app.route('/hello')
+hello = Blueprint('hello', __name__)
+
+
+@hello.route('/')
+@hello.route('/hello')
 def hello_world():
     return MESSAGES['default']
 
 
-@app.route('/show/<key>')
+@hello.route('/show/<key>')
 def get_message(key):
     return MESSAGES.get(key) or "%s not found!" % key
 
 
-@app.route('/add/<key>/<message>')
+@hello.route('/add/<key>/<message>')
 def add_or_update_message(key, message):
     MESSAGES[key] = message
     return "%s Added/Updated" % key
