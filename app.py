2829d44794c3a0ca4144230d89190dc7cb74822b
Shalabh Aggarwal <shalabh7777@gmail.com>
2014-09-14 20:21:04 +0530

    Chapter 1 - Recipe 'Handling basic configurations (Added debug to config)'

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route('/')
 def hello_world():
     return 'Hello to the World of Flask!'
 
 if __name__ == '__main__':
-    app.debug = True
+    #app.debug = True
     app.run()
